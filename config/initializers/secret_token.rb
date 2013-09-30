93dc149d6602643f5be284e2025a5602f4949780
John Splawn <jsplawn@gmail.com>
2013-09-30 15:17:32 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beergo2::Application.config.secret_key_base = '360a3fd66be8d417c06bcf8b287fb6c2935c98418ca2cbf0f9c8f1b0f8ebed7668aa8a6142535a4b1552189bc1ec2cf0a6d8bccda08d0383ea6c3dc31c1e8a3c'
